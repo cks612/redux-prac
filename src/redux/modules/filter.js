57228139259cb835b90745a47b6d51578dee8124
cks612 <kyusung612@gmail.com>
2022-12-09 19:51:40 +0900

    redux actions added

--- src/redux/modules/filter.js
+++ src/redux/modules/filter.js
@@ -1,26 +1,46 @@
+import { createActions, handleActions } from "redux-actions";
+
+export const { showAll, showComplete } = createActions(
+  "SHOW_ALL",
+  "SHOW_COMPLETE",
+  {
+    prefix: "redux-start/filter",
+  }
+);
+
 // action type 정의
-const SHOW_ALL = "redux-start/filter/SHOW_ALL";
-const SHOW_COMPLETE = "redux-start/filter/SHOW_COMPLETE";
+// const SHOW_ALL = "redux-start/filter/SHOW_ALL";
+// const SHOW_COMPLETE = "redux-start/filter/SHOW_COMPLETE";
 
 // action 생성 함수
-export function showAll() {
-  return { type: SHOW_ALL };
-}
-export function showComplete() {
-  return { type: SHOW_COMPLETE };
-}
+// export function showAll() {
+//   return { type: SHOW_ALL };
+// }
+// export function showComplete() {
+//   return { type: SHOW_COMPLETE };
+// }
 
 // 초기값
 const initialState = "ALL";
 
-// 리듀서
-export default function reducer(previousState = initialState, action) {
-  if (action.type === SHOW_COMPLETE) {
-    return "COMPLETE";
-  }
-  if (action.type === SHOW_ALL) {
-    return "ALL";
-  }
+const reducer = handleActions(
+  {
+    SHOW_ALL: (state, action) => "ALL",
+    SHOW_COMPLETE: () => "COMPLETE",
+  },
+  initialState,
+  { prefix: "redux-start/filter" }
+);
 
-  return previousState;
-}
+// 리듀서
+export default reducer;
+// export default function reducer(previousState = initialState, action) {
+//   if (action.type === showComplete) {
+//     return "COMPLETE";
+//   }
+//   if (action.type === showAll) {
+//     return "ALL";
+//   }
+
+//   return previousState;
+// }
